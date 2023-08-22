736accbaab36a93c4ce424cc651b3e1dbbbd65e0
WilliamCross <WilliamCross.CS@gmail.com>
2023-08-22 16:55:58 -0400

    Finished video 7 of TechieSyed's MVVM tutorials

---
+++ MVVM-TechieSyed/MVVM-TechieSyed/Models/EmployeeService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_TechieSyed.Models
+{
+    public class EmployeeService
+    {
+        private static ObservableCollection<Employee> ObjEmployeesList;
+        public EmployeeService()
+        {
+            ObjEmployeesList = new ObservableCollection<Employee>()
+            {
+                new Employee{Id = 101, Name = "Will", Age = 22}
+            };
+        }
+        public ObservableCollection<Employee> GetAll()
+        {
+            return ObjEmployeesList;
+        }
+        public bool Add(Employee objNewEmployee)
+        {
+            // Age must be between 21 and 58
+            if(objNewEmployee.Age < 21 || objNewEmployee.Age > 58)
+            {
+                throw new ArgumentException("Invalid age limit for employee");
+            }
+            ObjEmployeesList.Add(objNewEmployee);
+            return true;
+        }
+
+        public bool Update(Employee objEmployeeToUpdate)
+        {
+            bool IsUpdated = false;
+            foreach (Employee objEmployee in ObjEmployeesList)
+            {
+                if (objEmployee.Id == objEmployeeToUpdate.Id)
+                {
+                    objEmployee.Age = objEmployeeToUpdate.Age;
+                    objEmployee.Name = objEmployeeToUpdate.Name;
+                    IsUpdated = true;
+                    break;
+                }
+            }
+            return IsUpdated;
+        }
+
+        public bool Delete(int id)
+        {
+            bool IsDeleted = false;
+            foreach (Employee objEmployee in ObjEmployeesList)
+            {
+                if (objEmployee.Id == id)
+                {
+                    ObjEmployeesList.Remove(objEmployee);
+                    IsDeleted = true;
+                    break;
+                }
+            }
+            return IsDeleted;
+        }
+
+        public Employee Search(int id)
+        {
+            return ObjEmployeesList.FirstOrDefault(e=>e.Id==id);
+        }
+    }
+}
---
+++ MVVM-TechieSyed/MVVM-TechieSyed/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MVVM_TechieSyed.Models;
+using MVVM_TechieSyed.Commands;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+namespace MVVM_TechieSyed.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged_Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+        EmployeeService ObjEmployeeService;
+        public EmployeeViewModel()
+        {
+            ObjEmployeeService = new EmployeeService();
+            LoadData();
+            CurrentEmployee = new Employee();
+            saveCommand = new RelayCommand(Save);
+        }
+
+        #region DisplayOperation
+        private ObservableCollection<Employee> employeesList;
+        public ObservableCollection<Employee> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value; OnPropertyChanged("EmployeesList"); }
+        }
+        private void LoadData()
+        {
+            EmployeesList = ObjEmployeeService.GetAll();
+        }
+        #endregion
+
+        private Employee currentEmployee;
+
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+        public void Save()
+        {
+            try
+            {
+                var IsSaved = ObjEmployeeService.Add(currentEmployee);
+                LoadData();
+                if(IsSaved == true)
+                {
+                    Message = "Employee saved";
+                } else
+                {
+                    Message = "Save Operation Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+
+    }
+}
